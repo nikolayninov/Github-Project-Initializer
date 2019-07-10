141930007cd41252246422cfc74748e45136b72c
nikolayninov <nikininov1@gmail.com>
2019-07-10 12:11:47 +0300

    Adding createProject.py

---
+++ createProject.py
@@ -0,0 +1,34 @@
+import sys
+import os
+from github import Github  # pip install PyGithub
+
+# Your Github access token
+ACCESS_TOKEN = "c96590efd0898e62fe0bbda17f7ee0195d5ef9b9"
+# Path to the folder where you want to create your projects
+PROJECTS_FOLDER = 'S:\Projects'
+
+OS = sys.platform
+
+project_name = sys.argv[1]
+g = Github(ACCESS_TOKEN)
+repo = g.get_user().create_repo(project_name)
+git_url = f'https://{repo.git_url[6:]}'
+
+# Changing directory to the projets folder
+os.chdir(PROJECTS_FOLDER)
+# Cloning the repo
+os.system(f'git clone {git_url}')
+# Changing directory to the repo's folder
+os.chdir(repo.name)
+# Creating README.md
+if OS.startswith('win'):
+    os.system('type nul > README.md')
+elif OS.startswith('linux') or OS.startswith('darwin'):
+    os.system('touch README.md')
+
+os.system('git add .')
+os.system('git commit -m "Initial commit"')
+os.system('git push -u origin master')
+
+# This line opens the folder of the project with VS Code (You can change it if you want)
+os.system('code .')
